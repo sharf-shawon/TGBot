bd13b2d16ea5b70787f17576c6a3acd14cdf4423
Sharfuddin Shawon @W11 <sharfuddin.shawon@gmail.com>
2026-02-15 09:23:15 -0600

    rename docker compose

--- src/core/env.py
+++ src/core/env.py
@@ -3,5 +3,4 @@ from pathlib import Path
 from dotenv import dotenv_values
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-ENV_FILE = BASE_DIR / ".env"
-ENV = dotenv_values(ENV_FILE)
+ENV = dotenv_values()
